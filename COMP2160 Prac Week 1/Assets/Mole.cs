718c18548f195f0917446b619c9ac35ee7a90150
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-07-28 19:19:49 +1000

    Finished

--- COMP2160 Prac Week 1/Assets/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Mole.cs
@@ -1,46 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(SpriteRenderer))]
 
 public class Mole : MonoBehaviour
 {
-    public SpriteRenderer Sprite;
+    private SpriteRenderer Sprite;
     public Color Down = new Color(1, 1, 1);
     public Color Up = new Color(1, 1, 0);
     public Color Missed = new Color(1, 0, 0);
-    private float timer = 5;
+    private float timer = 3;
+    private float randomTimer;
     
 
     // Start is called before the first frame update
     void Start()
     {
         Sprite = GetComponent<SpriteRenderer>();
-        Sprite.color = Up;
+        randomTimer = Random.Range(2f, 10f);
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        if (Sprite.color == Down)
+        {
+            Debug.Log("rand " + randomTimer);
+            randomTimer -= Time.deltaTime;
+            
+            if(randomTimer <= 0)
+            {
+                Sprite.color = Up;
+                randomTimer = Random.Range(2f, 10f);
+            }
+        }
         if(Sprite.color == Up)
         {
             timer -= Time.deltaTime;
             if (timer <= 0)
             {
                 Sprite.color = Missed;
-                timer = 3;
+                timer = 2;
             }
         }
-
         if(Sprite.color == Missed)
         {
             timer -= Time.deltaTime;
             if(timer <= 0)
             {
                 Sprite.color = Down;
-                timer = 5;
+                timer = 3;
             }
         }
         
